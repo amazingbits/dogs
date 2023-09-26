1e1fc8efc9184256d8fa02b0bc8c831df283048e
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-26 16:42:55 -0300

    0808 - useForm

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-function Input({ label, type, name }) {
+function Input({ label, type, name, value, onChange, error, onBlur }) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input type={type} name={name} id={name} className={styles.input} />
-      <p className={styles.error}>Error</p>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        className={styles.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 }
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../hooks/useForm';
 
 function LoginForm() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const apiRequest = await fetch(
-      'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    if (username.validate() && password.validate()) {
+      const apiRequest = await fetch(
+        'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
         },
-        body: JSON.stringify({ username, password }),
-      },
-    );
-    const apiRequestJson = await apiRequest.json();
-    console.log(apiRequestJson);
+      );
+      const apiRequestJson = await apiRequest.json();
+      console.log(apiRequestJson);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input type="text" label="Usuário" name="username" />
-        <Input type="password" label="Senha" name="password" />
+        <Input type="text" label="Usuário" name="username" {...username} />
+        <Input type="password" label="Senha" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
