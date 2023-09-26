2b0c25d3ff51e088ecc29f39b207407be61f4714
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-26 13:00:58 -0300

    0804 - Estrutura Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div>App React</div>
-  )
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
