958c5d0ead829c5f70db79d34ce4af7e3e7d6d66
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 14:44:17 -0300

    0832 - Perdeu Senha 1

--- src/api.jsx
+++ src/api.jsx
@@ -108,3 +108,16 @@ export function COMMENT_POST(id, body) {
     },
   };
 }
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: `${API_URL}/api/password/lost`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
--- src/components/Login/LoginPasswordLost.jsx
+++ src/components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
+import Error from '../Helper/Error';
 
 function LoginPasswordLost() {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+  const { data, error, loading, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('perdeu', 'resetar'),
+      });
+      await request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="E-mail / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar E-mail</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
 }
 
 export default LoginPasswordLost;
