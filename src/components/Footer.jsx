9e45d0cb4f26d16fd1ee58120b6fabbd5c937ba9
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 16:13:16 -0300

    0834 - Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { ReactComponent as Dogs } from '../assets/dogs-footer.svg';
 
 function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs | Alguns direitos reservados.</p>
+    </footer>
+  );
 }
 
 export default Footer;
