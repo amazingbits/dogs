eab5e8797df18ce52495070ea3c3c7b9ae82c13e
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 16:38:19 -0300

    0835 - Biblioteca Externa 1

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <>
-      <Head
-        title="Estatísticas"
-        description="Página de estatísticas do site Dogs."
-      />
-      <div>UserStats</div>
-    </>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <>
+        <Head
+          title="Estatísticas"
+          description="Página de estatísticas do site Dogs."
+        />
+        <div>
+          <UserStatsGraphs data={data} />
+        </div>
+      </>
+    );
+  else return null;
 };
 
 export default UserStats;
