380fb23c3d7088de506eb54e70815f9112e1c9f6
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 15:05:16 -0300

    0832 - Perdeu Senha 2

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -10,7 +10,7 @@ import useFetch from '../../hooks/useFetch';
 function LoginCreate() {
   const username = useForm();
   const email = useForm('email');
-  const password = useForm('password');
+  const password = useForm();
   const { userLogin } = React.useContext(UserContext);
   const { loading, error, request } = useFetch();
 
