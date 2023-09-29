f86f6308a83ba331b48752682853e959c8f767d6
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 10:58:47 -0300

    0824 - Foto Delete

---
+++ src/components/Photo/PhotoDelete.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../hooks/useFetch';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm(
+      'Tem certeza que deseja remover este arquivo?',
+    );
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { apiResponse } = await request(url, options);
+      if (apiResponse.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
