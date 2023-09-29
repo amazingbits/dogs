1f3fb66715d9ae062f2da51491f55faaa352da97
Guilherme Andrade <gui-keanne@hotmail.com>
2023-09-29 13:19:39 -0300

    0827 - Scroll Infinito

--- src/components/Feed/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos.jsx
@@ -6,19 +6,25 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ setModalPhoto, user, page, setInfinite }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      await request(url, options);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { apiResponse, apiResponseJson } = await request(url, options);
+      if (apiResponse && apiResponse.ok && apiResponseJson.length < total) {
+        setInfinite(false);
+      }
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
+  if (!loading && data && data.length === 0)
+    return <p className={styles.notFound}>Todos os itens foram carregados</p>;
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
